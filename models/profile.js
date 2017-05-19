4babdc02f24f7db8beb14345f5c0e47216732ce4
Justin Rhoads <rhoadsjustin@gmail.com>
2017-05-19 11:15:42 -0500

    setup so i can document my api endpoints

---
+++ models/profile.js
@@ -0,0 +1,10 @@
+// var mongoose = require('mongoose'),
+//   Schema = mongoose.Schema;
+//
+//   var ProfileSchema = new Schema ({
+//     name: String,
+//     githubUsername: String,
+//     githubProfileImage: String,
+//     personalSiteLink: String,
+//     currentCity: String,
+//   })
--- server.js
+++ server.js
@@ -46,14 +46,14 @@ app.get('/api', function apiIndex(req, res) {
   // TODO: Document all your api endpoints below as a simple hardcoded JSON object.
   // It would be seriously overkill to save any of this to your database.
   res.json({
-    woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
+    IRemeberedToDocumentAllMyEndpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express-personal-api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentationUrl: "https://github.com/rhoadsjustin/express-personal-api/README.md", // CHANGE ME
+    baseUrl: "http://glacial-brushlands-72768.herokuapp.com", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "POST", path: "/api/restaurants", description: "Add a new favorite restaurant"} // CHANGE ME
     ]
   })
 });
