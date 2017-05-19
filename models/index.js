6998eaefb2968fb0e756ea049bbab785523b23ed
Justin Rhoads <rhoadsjustin@gmail.com>
2017-05-19 11:56:08 -0500

    fixed syntax

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
 
-module.exports.Profile = require("./profile.js");
+module.exports.Profile = require("./project.js");
